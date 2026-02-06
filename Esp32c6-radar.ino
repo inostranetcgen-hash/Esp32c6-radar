b6d4af3353bb400a0a3c02e18e0a17c48a6aeab5
inostranetcgen-hash <inostranetcgen@gmail.com>
2026-02-06 12:55:44 +0500

    Esp32c6-radar.ino

--- Esp32c6-radar.ino
+++ Esp32c6-radar.ino
@@ -1,26 +1,9 @@
 #define SERVO_PIN 18
 
-int currentPulse = 1500;   // старт с центра
-int minPulse = 1200;       // -90°
-int maxPulse = 1800;       // +90°
-int step = 5;              // МЕНЬШЕ = МЕДЛЕННЕЕ
-int stepDelay = 25;        // МЕНЬШЕ = БЫСТРЕЕ
-
 void pulse(int us) {
   digitalWrite(SERVO_PIN, HIGH);
   delayMicroseconds(us);
   digitalWrite(SERVO_PIN, LOW);
-  delay(20);
-}
-
-void moveTo(int target) {
-  while (currentPulse != target) {
-    if (currentPulse < target) currentPulse += step;
-    if (currentPulse > target) currentPulse -= step;
-
-    pulse(currentPulse);
-    delay(stepDelay);
-  }
 }
 
 void setup() {
@@ -28,9 +11,6 @@ void setup() {
 }
 
 void loop() {
-  moveTo(minPulse);  // влево
-  delay(500);
-
-  moveTo(maxPulse);  // вправо
-  delay(500);
+  pulse(1500);     // центр / стоп для continuous
+  delay(20);       // 50 Гц
 }
