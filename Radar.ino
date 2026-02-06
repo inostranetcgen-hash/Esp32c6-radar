8061e04c21860c1e3c72ce67b2ae91fa141f29ee
inostranetcgen-hash <inostranetcgen@gmail.com>
2026-02-06 12:01:27 +0500

    Radar.ino

--- Radar.ino
+++ Radar.ino
@@ -1,103 +1,30 @@
-#include <WiFi.h>
-#include <WebServer.h>
-#include <Servo.h>
-
 #define SERVO_PIN 18
-#define RADAR_OUT_PIN 0
+#define SERVO_CHANNEL 0
+#define SERVO_FREQ 50
+#define SERVO_RESOLUTION 16
 
-Servo radarServo;
-WebServer server(80);
+static const uint32_t dutyMin = 3277; // ~0.5ms
+static const uint32_t dutyMax = 6554; // ~2.5ms
 
-int angle = 0;
-bool forward = true;
+void setup() {
+  Serial.begin(115200);
+  Serial.println("ESP32-C6 servo test (LEDC)");
 
-const char PAGE[] PROGMEM = R"rawliteral(
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>ESP32 Radar</title>
-<style>
-body { background:black; color:lime; text-align:center; }
-canvas { background:black; }
-</style>
-</head>
-<body>
-<h2>RADAR ONLINE</h2>
-<canvas id="radar" width="300" height="150"></canvas>
-<script>
-const c = document.getElementById("radar");
-const ctx = c.getContext("2d");
-const r = 140;
+  ledcSetup(SERVO_CHANNEL, SERVO_FREQ, SERVO_RESOLUTION);
+  ledcAttachPin(SERVO_PIN, SERVO_CHANNEL);
+}
 
-function draw(angle, detected) {
-  ctx.clearRect(0,0,300,150);
-  ctx.strokeStyle="lime";
-  ctx.beginPath();
-  ctx.arc(150,150,r,Math.PI,2*Math.PI);
-  ctx.stroke();
-
-  let a = angle * Math.PI / 180;
-  ctx.beginPath();
-  ctx.moveTo(150,150);
-  ctx.lineTo(150 + r*Math.cos(Math.PI+a), 150 + r*Math.sin(Math.PI+a));
-  ctx.stroke();
-
-  if(detected){
-    ctx.fillStyle="red";
-    ctx.beginPath();
-    ctx.arc(
-      150 + (r-20)*Math.cos(Math.PI+a),
-      150 + (r-20)*Math.sin(Math.PI+a),
-      6,0,2*Math.PI);
-    ctx.fill();
+void loop() {
+  for (uint32_t duty = dutyMin; duty <= dutyMax; duty += 40) {
+    ledcWrite(SERVO_CHANNEL, duty);
+    delay(20);
   }
-}
+  delay(300);
 
-setInterval(async ()=>{
-  let r = await fetch("/data");
-  let j = await r.json();
-  draw(j.angle, j.detected);
-},200);
-</script>
-</body>
-</html>
-)rawliteral";
-
-void handleRoot(){
-  server.send_P(200,"text/html",PAGE);
-}
-
-void handleData(){
-  String json = "{";
-  json += "\"angle\":" + String(angle) + ",";
-  json += "\"detected\":" + String(digitalRead(RADAR_OUT_PIN));
-  json += "}";
-  server.send(200,"application/json",json);
-}
-
-void setup(){
-  pinMode(RADAR_OUT_PIN, INPUT);
-  radarServo.attach(SERVO_PIN);
-
-  WiFi.softAP("ESP32-RADAR","12345678");
-
-  server.on("/",handleRoot);
-  server.on("/data",handleData);
-  server.begin();
-}
-
-void loop(){
-  server.handleClient();
-
-  radarServo.write(angle);
-  delay(20);
-
-  if(forward){
-    angle++;
-    if(angle>=180) forward=false;
-  }else{
-    angle--;
-    if(angle<=0) forward=true;
+  for (uint32_t duty = dutyMax; duty >= dutyMin; duty -= 40) {
+    ledcWrite(SERVO_CHANNEL, duty);
+    delay(20);
+    if (duty < dutyMin + 40) break;
   }
+  delay(300);
 }
