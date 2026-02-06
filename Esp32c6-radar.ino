adf95d15d8ba9be01789665e36d83d1bc92ceb58
inostranetcgen-hash <inostranetcgen@gmail.com>
2026-02-07 00:14:11 +0500

    Esp32c6-radar.ino

--- Esp32c6-radar.ino
+++ Esp32c6-radar.ino
@@ -1,5 +1,14 @@
 #define SERVO_PIN 18
-int CENTER_US = 1498;
+
+int CENTER_US = 1499;
+int SPAN_US   = 50;
+
+int STEP_US   = 3;   // 2..4 обычно идеально; 1 часто даёт дрейф
+int cur;
+int dir = +1;
+
+inline int minPulse() { return CENTER_US - SPAN_US; }
+inline int maxPulse() { return CENTER_US + SPAN_US; }
 
 void pulse_50hz(int us) {
   digitalWrite(SERVO_PIN, HIGH);
@@ -8,8 +17,29 @@ void pulse_50hz(int us) {
   delayMicroseconds(20000 - us);
 }
 
-void setup() { pinMode(SERVO_PIN, OUTPUT); }
+void setup() {
+  pinMode(SERVO_PIN, OUTPUT);
+  cur = CENTER_US;
+  // короткая стабилизация
+  for (int i = 0; i < 10; i++) pulse_50hz(cur);
+}
 
 void loop() {
-  pulse_50hz(CENTER_US);   // всё время один и тот же импульс
+  pulse_50hz(cur);
+
+  cur += dir * STEP_US;
+
+  if (cur >= maxPulse()) {
+    cur = maxPulse();
+    dir = -1;
+    // микродемпфер: 2 кадра (~40мс) без заметной паузы
+    pulse_50hz(cur);
+    pulse_50hz(cur);
+  } 
+  else if (cur <= minPulse()) {
+    cur = minPulse();
+    dir = +1;
+    pulse_50hz(cur);
+    pulse_50hz(cur);
+  }
 }
